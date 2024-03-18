d65ef49aaf0d2a0ab930e890e6116fc7161649bb
Pratham Mehta <pratham.mehta0826@gmail.com>
2024-03-18 06:05:39 +0000

    changes

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import {router} from "./controlllers/post.js";
+import bodyParser from "body-parser";
 const app = express();
 const port = process.env.PORT || 8000;
 
 app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
 app.use(express.json({
     limit: '50mb'
 }));
